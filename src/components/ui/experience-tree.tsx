03397de8159e505150484393d250c892874b2f37
Youssef Elfaramawy <yelfaram@gmail.com>
2025-07-14 13:20:03 -0400

    Added icons for file in magicui

--- src/components/ui/experience-tree.tsx
+++ src/components/ui/experience-tree.tsx
@@ -1,4 +1,5 @@
 import { File, Folder, Tree } from "@/components/magicui/file-tree";
+import { GraduationCap, Briefcase, Landmark } from "lucide-react";
 
 // Experience & Education Section
 export function ExperienceTreeDemo() {
@@ -20,14 +21,35 @@ export function ExperienceTreeDemo() {
       >
         <Folder element="Education" value="education">
           <Folder element="University of Ottawa" value="uottawa">
-            <File value="masters">MEng Electrical & Computer Engineering</File>
-            <File value="bachelors">BASc Computer Engineering</File>
+            <File
+              value="masters"
+              fileIcon={
+                <GraduationCap className="w-5 h-5 text-[#2d2d2c] dark:text-[#8f001a]" />
+              }
+            >
+              <div className="text-left ml-1">
+                MEng Electrical & Computer Engineering
+              </div>
+            </File>
+            <File
+              value="bachelors"
+              fileIcon={
+                <GraduationCap className="w-5 h-5 text-[#2d2d2c] dark:text-[#8f001a]" />
+              }
+            >
+              <div className="text-left ml-1">BASc Computer Engineering</div>
+            </File>
           </Folder>
         </Folder>
         <Folder element="Experience" value="experience">
           <Folder element="Mitel Networks Corporation" value="mitel">
-            <File value="mitel-2022">
-              <div className="text-left ml-2">
+            <File
+              value="mitel-2022"
+              fileIcon={
+                <Briefcase className="w-5 h-5 text-[#0d4496] dark:text-[#00A1F4]" />
+              }
+            >
+              <div className="text-left ml-1">
                 Software Developer Co-op
                 <br />
                 <div className="text-slate-600 dark:text-slate-300">
@@ -35,8 +57,13 @@ export function ExperienceTreeDemo() {
                 </div>
               </div>
             </File>
-            <File value="mitel-2021">
-              <div className="text-left ml-2">
+            <File
+              value="mitel-2021"
+              fileIcon={
+                <Briefcase className="w-5 h-5 text-[#0d4496] dark:text-[#00A1F4]" />
+              }
+            >
+              <div className="text-left ml-1">
                 Software Developer Co-op
                 <br />
                 <div className="text-slate-600 dark:text-slate-300">
@@ -46,7 +73,12 @@ export function ExperienceTreeDemo() {
             </File>
           </Folder>
           <Folder element="Transport Canada" value="transport-canada">
-            <File value="transport-canada-2021" className="">
+            <File
+              value="transport-canada-2021"
+              fileIcon={
+                <Landmark className="w-5 h-5 text-[#EB2D37] dark:text-white" />
+              }
+            >
               <div className="text-left ml-2">
                 Application Developer Co-op
                 <div className="text-slate-600 dark:text-slate-300">
